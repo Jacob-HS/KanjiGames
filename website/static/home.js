ecde3e2aa88b835efe4bc7d8d7af6423254ae6e8
Jacob-HS <jacobsabbagh99@gmail.com>
2023-03-27 15:44:59 +0900

    Added gameplay capability for beta testing

--- website/static/home.js
+++ website/static/home.js
@@ -1,4 +1,1 @@
-let pbois = document.getElementById("myContainer").children
-    for (let i = 0; i<pbois.length;i++){
-      pbois[i].style.animationDelay=i*.15+"s";
-    }
+let pbois = document.getElementById("myContainer").children;
--- website/static/radicalRush.js
+++ website/static/radicalRush.js
@@ -1,9 +1,18 @@
-let difficulty=0;
-let pbois = document.getElementById("myContainer").children;
-const masterSheet = JSON.parse("{{ componentsToKanji }}");
-alert(masterSheet)
-for (let i = 0; i<pbois.length;i++){
-  pbois[i].style.animationDelay=i*.15+"s";
+var currentQuestion="";
+var askedPool = new Set();
+var answer=document.getElementById("answer");
+var keys = Object.keys(masterList);
+answer.addEventListener("keypress", function(event){
+  if(event.key=="Enter"){
+    checkAnswer();
+    console.log("hello")
+  }
+})
+function tickDown(){
+  document.getElementById("timer").innerHTML=parseInt(document.getElementById("timer").innerHTML)-1;
+}
+function addTime(){
+  document.getElementById("timer").innerHTML=parseInt(document.getElementById("timer").innerHTML)+10;
 }
 function makeGoAway(diff){
   let diffh = document.getElementById("diffh");
@@ -23,5 +32,31 @@ function startGame(diff){
   for (let element of gameplayElements){
     element.className="gameplayElement active";
   }
+  document.getElementById("timer").innerHTML=10;
+  setInterval(tickDown,1000);
+  pickQuestion();
+  document.getElementById("answer").focus();
+}
+function pickQuestion(){
+  do{
+    candidate= keys[ 50 * Math.random() << 0];
+  } while (askedPool.has(candidate));
+  askedPool.add(candidate);
+  document.getElementById("currRadical").innerHTML=candidate;
+  currentQuestion=candidate;
+} 
 
+
+function checkAnswer(){
+  var answerElement=document.getElementById("answer");
+  var answer=answerElement.value;
+  answerElement.value="";
+  for(const kanji of answer){
+    if (masterList[currentQuestion].includes(kanji)){
+      document.getElementById("score").innerHTML=1+parseInt(document.getElementById("score").innerHTML);
+      pickQuestion();
+      addTime();
+      break;
+    }
+  }
 }
