f5ebf6ffa9fd33913078a1bb952dec7cc299c128
Jacob-HS <jacobsabbagh99@gmail.com>
2023-03-29 16:05:16 +0900

    Added summary page and updated question pool

--- website/static/radicalRush.js
+++ website/static/radicalRush.js
@@ -1,16 +1,19 @@
 var currentQuestion="";
 var poolSize;
-var askedPool = new Set();
-var answer=document.getElementById("answer");
+var addedListener =false;
+var askedPool =[];
 var keys = Object.keys(masterList);
+var intervalID;
 answer.addEventListener("keypress", function(event){
   if(event.key=="Enter"){
     checkAnswer();
-    console.log("hello")
   }
 })
 function tickDown(){
   document.getElementById("timer").innerHTML=parseInt(document.getElementById("timer").innerHTML)-1;
+  if (document.getElementById("timer").innerHTML =="0"){
+    endGame(false);
+  }
 }
 function addTime(){
   document.getElementById("timer").innerHTML=parseInt(document.getElementById("timer").innerHTML)+10;
@@ -21,31 +24,35 @@ function makeGoAway(diff){
   diffh.classList.add("goAway");
   container.classList.add("goAway");
   poolSize=document.getElementById("diffChanger").value;
-  document.getElementById("label").classList.add("goAway");
-  document.getElementById("diffChanger").classList.add("goAway");
-  diffh.addEventListener("transitionend", () =>
-  {
-    diffh.remove();
-    container.remove();
-    startGame(diff);
-  })
+  document.getElementById("label").style.display="none";
+  document.getElementById("diffChanger").style.display="none";
+  if (!addedListener){
+    addedListener=true;
+    diffh.addEventListener("transitionend", () =>
+    {
+      diffh.classList.add("hidden");
+      container.classList.add("hidden");
+      startGame();
+    })
+  }
 }
-function startGame(diff){
+function startGame(){
   let gameplayElements = document.getElementsByClassName("gameplayElement");
   for (let element of gameplayElements){
     element.classList.remove("hidden");
     element.classList.add("active");
   }
   document.getElementById("timer").innerHTML=10;
-  setInterval(tickDown,1000);
+  console.log("start");
+  intervalID = setInterval(tickDown,1000);
   pickQuestion();
   document.getElementById("answer").focus();
 }
 function pickQuestion(){
   do{
     candidate= keys[ poolSize * Math.random() << 0];
-  } while (askedPool.has(candidate));
-  askedPool.add(candidate);
+  } while (askedPool.includes(candidate));
+  askedPool.push(candidate);
   document.getElementById("currRadical").innerHTML=candidate;
   currentQuestion=candidate;
 } 
@@ -58,13 +65,97 @@ function checkAnswer(){
   for(const kanji of answer){
     if (kanji == "ｓ" || kanji == "s"){
       pickQuestion();
-      break;
+      return;
     }
     if (masterList[currentQuestion].includes(kanji)){
       document.getElementById("score").innerHTML=1+parseInt(document.getElementById("score").innerHTML);
+      if (document.getElementById("score").innerHTML=="10"){
+        endGame(true);
+        return;
+      }
       pickQuestion();
       addTime();
       break;
     }
   }
+}
+function endGame(win){
+  console.log("stop");
+  clearInterval(intervalID);
+  let gameplayElements = document.getElementsByClassName("gameplayElement");
+  for (let element of gameplayElements){
+    element.classList.remove("active");
+    element.classList.add("hidden");
+  }
+  displaySummary(win);
+}
+
+function displaySummary(win){
+  result=document.getElementById("result");
+  if (win){
+    result.innerHTML="Victory!";
+  }else {
+    result.innerHTML="Defeat."
+  }
+  let summaryElements = document.getElementsByClassName("summaryElement");
+  for (let element of summaryElements){
+    element.classList.remove("hidden");
+    element.classList.add("active");
+  }
+
+
+  generateSummaryAnswers();
+ 
+}
+function generateSummaryAnswers(){
+  var row;
+  var temp;
+  var kanji;
+  summaryAnswerContainer=document.getElementById("summaryAnswerContainer");
+  for (let component of askedPool){
+    console.log(askedPool);
+    row=document.createElement("div");
+    temp=document.createElement("p");
+    kanji=document.createTextNode(component);
+    temp.appendChild(kanji);
+    temp.classList.add("summaryElement","active","summaryComponent");
+    row.appendChild(temp);
+    summaryAnswerContainer.appendChild(row);
+    row.classList.add("summaryElement","active", "summaryAnswerRow");
+
+      for(let i=0;i<10;i++){
+        temp=document.createElement("p");
+        kanji=document.createTextNode(masterList[component][i]);
+        temp.appendChild(kanji);
+        temp.classList.add("summaryElement","active","summaryAnswer");
+        row.appendChild(temp);
+
+      }
+
+
+
+  }
+}
+
+function resetGame(){
+  let summaryElements = document.getElementsByClassName("summaryElement");
+  for (let element of summaryElements){
+    element.classList.remove("active");
+    element.classList.add("hidden");
+  }
+  askedPool=[];
+  document.getElementById("score").innerHTML=0;
+}
+function playAgain(){
+  resetGame();
+  startGame();
+}
+function changeDifficulty(){
+  resetGame();
+  let diffh = document.getElementById("diffh");
+  let container= document.getElementById("myContainer");
+  diffh.classList.remove("goAway","hidden");
+  container.classList.remove("goAway","hidden");
+
+
 }
