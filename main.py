e48702c4322c05ef1ab9dfcb3ae945ea7e09cef8
Jacob-HS <jacobsabbagh99@gmail.com>
2023-04-14 17:32:10 +0900

    added socket io connection

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from website import create_app
-
+from flask_socketio import SocketIO, emit
+import random
 app = create_app()
+socketio = SocketIO(app)
+
+@socketio.on("my_event")
+def my_event(message):
+    print("im in bois")
+    emit('my_response',
+         {'data': "harro"})
+
+@socketio.event
+def generateKey():
+    return random.randint(1,100000000)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
