c2ab6e5cdf656da78a11c4af27fe9ededc1b270c
Jacob-HS <jacobsabbagh99@gmail.com>
2023-04-17 16:57:54 +0900

    passing over

--- website/static/drawnOutDuel.js
+++ website/static/drawnOutDuel.js
@@ -1,6 +1,7 @@
 (() => {
   let roomNum;
   let host;
+  let intervalID;
 var socket = io();
 socket.on('connect', function() {
   socket.emit('my_event', {data: 'I\'m connected!'});
@@ -46,6 +47,7 @@ socket.on("pickedName", (namae)=>{
 
 socket.on("startGame", function(){
   clearField();
+  startCountdown();
 });
 
 function checkName(){
@@ -89,6 +91,96 @@ function copyInvite(){
 function clearField(){
   document.getElementById("roomCreationContainer").classList.add("hidden");
   
+}
+
+function startCountdown(){
+  countdownTimer = document.getElementById("countdownTimer");
+  countdownTimer.classList.remove("hidden");
+  countdownTimer.classList.add("active");
+  countdownTimer.innerHTML="3";
+  intervalID=setInterval(tickDown, 1000);
+}
+function tickDown(){
+  let countdownTimer = document.getElementById("countdownTimer");
+  countdownTimer.innerHTML=parseInt(countdownTimer.innerHTML)-1;
+  if (countdownTimer.innerHTML =="0"){
+    clearInterval(intervalID);
+    countdownTimer.classList.remove("active");
+    countdownTimer.classList.add("hidden");
+    toggleGameElements();
+    pickQuestion();
+  }
+}
+
+function pickQuestion(){
+  svgContainer = document.getElementById("svg-container");
+  svgContainer.innerHTML="";
+  currentQuestion=keys[Math.floor(Math.random()*poolSize)];
+  askedPool.push(currentQuestion);
+  for (const kanji of currentQuestion){
+    svgContainer.innerHTML=svgContainer.innerHTML+masterList[kanji];
+  }
+  smushSvgs();
+  hidePaths();
+  setPathAppearTime();
+}
+
+function smushSvgs(){
+  svgs = document.getElementsByTagName("svg");
+  wordLength=svgs.length;
+  const viewBoxBoi=
+  [["-5 0 109 109","5 0 109 109"],
+  ["-5 0 109 109","0 0 109 109","5 0 109 109"],
+  ["-15 0 109 109","-5 0 109 109","5 0 109 109","15 0 109 109"],
+  ["-20 0 109 109","-10 0 109 109","0 0 109 109","10 0 109 109","20 0 109 109"]
+  ];
+  let i=0;
+  for (const svg of svgs){
+    svg.setAttribute("viewBox",viewBoxBoi[wordLength-2][i]);
+    i++;
+  }
+}
+function startPathAppearance(){
+  paths = document.getElementsByTagName("path");
+  for (const path of paths){
+    path.classList.add("activePath");
+  }
+}
+
+function setPathAppearTime(){
+  console.log("start of set path");
+  paths = document.getElementsByTagName("path");
+  let i=0;
+  let j=1;
+  //request random array of paths.length size
+  //arr=getRandomArray(paths.length);
+  firstHalf=5/(parseFloat(paths.length)*.4);
+  secondHalf=15/(parseFloat(paths.length)*.6);
+  console.log(firstHalf);
+  console.log(secondHalf);
+  for (const num of arr){
+    if (i<(paths.length)/2){
+      paths[num].style.transitionDelay=+firstHalf*i+"s";
+      i++;
+    } else{
+      paths[num].style.transitionDelay=firstHalf*(i-1)+(secondHalf*(j))+"s";
+      j++;
+    }
+    
+  }
+  console.log("end of set path");
+}
+function toggleGameElements(){
+  gameElements=document.getElementsByClassName("gameplayElement");
+  for (const element of gameElements) {
+    if(element.classList.contains("active")){
+      element.classList.remove("active");
+      element.classList.add("hidden");
+    }else{
+      element.classList.add("active");
+      element.classList.remove("hidden");
+    }
+  }
 }
 
 })();
